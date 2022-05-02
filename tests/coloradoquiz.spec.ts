2e29bd7217c1a9cab86087f09498e08c87ad2f1d
Kushal Hada <kus@tilde.team>
2022-05-02 15:00:55 -0400

    add a test repo

---
+++ tests/coloradoquiz.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://coloradoquiz.web.app');
+});
+
+test.describe('Load page', () => {
+  test('should load the page', async ({ page }) => {
+    console.info({ page });
+  });
+});
